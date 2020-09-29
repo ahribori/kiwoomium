2907f1b8d69b86d9fa6d239c1bddb533716ce5e4
Daniel <ahribori@gmail.com>
2020-09-29 17:57:06 +0900

    ADD webview setting

--- ui/webview.py
+++ ui/webview.py
@@ -1,6 +1,13 @@
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 
 
 class WebView(QWebEngineView):
     def __init__(self):
         QWebEngineView.__init__(self)
+        settings = self.settings()
+        settings.setAttribute(QWebEngineSettings.LocalStorageEnabled, True)
+        settings.setAttribute(QWebEngineSettings.DnsPrefetchEnabled, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptCanAccessClipboard, True)
+        settings.setAttribute(QWebEngineSettings.Accelerated2dCanvasEnabled, True)
+        settings.setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
